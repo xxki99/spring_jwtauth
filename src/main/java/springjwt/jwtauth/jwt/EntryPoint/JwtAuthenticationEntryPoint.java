62d5a087cbf7b48c638a671da88f30f1c63a892e
xxki99 <alselearning@gmail.com>
2021-04-02 17:58:57 +0800

    Add Entrypoint for jwt auth

---
+++ src/main/java/springjwt/jwtauth/jwt/EntryPoint/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,46 @@
+package springjwt.jwtauth.jwt.EntryPoint;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+    Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        
+        logger.error("Unauthorized Error {}", authException.getMessage());
+
+        
+
+        SecurityContext sc = SecurityContextHolder.getContext();
+        Authentication principal = sc.getAuthentication();
+        if (principal == null){
+            logger.info("Authenication is null");
+        }
+        else{
+            logger.info("principal = " + principal.getPrincipal());
+        }
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: CustomEntrypointUnauthorized");
+
+        
+    }
+    
+}
