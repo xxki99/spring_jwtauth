a2c160860a8425c019b38470543b061ef8887b7b
xxki99 <alselearning@gmail.com>
2021-04-02 17:57:31 +0800

    Add filter

--- src/main/java/springjwt/jwtauth/jwt/Configuration/JwtWebSecurityConfig.java
+++ src/main/java/springjwt/jwtauth/jwt/Configuration/JwtWebSecurityConfig.java
@@ -1,21 +1,50 @@
 package springjwt.jwtauth.jwt.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import springjwt.jwtauth.jwt.EntryPoint.JwtAuthenticationEntryPoint;
+import springjwt.jwtauth.jwt.Filter.JwtAuthFilter;
+import springjwt.jwtauth.jwt.Utils.JwtUtils;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JwtWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public JwtUtils jwtUtils(){
+        return new JwtUtils();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public JwtAuthFilter jwtFilter(){
+        return new JwtAuthFilter();
+    }
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,13 +53,17 @@ public class JwtWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .csrf()
                 .disable()
+            .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
             .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
             .authorizeRequests()
                 .antMatchers("/api/register").permitAll()
+                .antMatchers("/api/login").permitAll()
                 .anyRequest().authenticated();
         
+        http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
             
     }
 }
---
+++ src/main/java/springjwt/jwtauth/jwt/Filter/JwtAuthFilter.java
@@ -0,0 +1,107 @@
+package springjwt.jwtauth.jwt.Filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import springjwt.jwtauth.jwt.Service.UserDetailsServiceImpl;
+import springjwt.jwtauth.jwt.Utils.JwtUtils;
+
+public class JwtAuthFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailsServiceImpl userService;
+
+    @Autowired
+    AuthenticationManager authManager;
+
+    // create Logger for loging message
+    private final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
+
+
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        // get jwt token header from request
+        Cookie[] cookies = request.getCookies();
+
+        String token = "";
+
+
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("Authorization")){
+
+                token = cookie.getValue();
+            }
+        }
+
+
+
+        // extract token from jwtTokenHeader
+        if (token != null){
+
+
+                try{
+                    if (jwtUtils.validateJwtToken(token)){
+
+                        UserDetails userDetails = userService.loadUserByUsername(jwtUtils.getUsernameFromToken(token));
+
+
+                        // create authenication object
+                        UsernamePasswordAuthenticationToken userAuth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        userAuth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                        SecurityContext sc = SecurityContextHolder.getContext();
+                        sc.setAuthentication(userAuth);
+
+                        
+                        logger.info("Set authentication");
+
+
+                    }
+
+
+
+                }
+                catch(NullPointerException e){
+                    // logger.error("NullPointerException in filter: " + e.getMessage());
+                }
+                catch(UsernameNotFoundException e){
+
+                    
+                    logger.info("Error occurs when trying authentication token: " + e.getMessage());
+                }
+                
+
+            
+        }
+
+
+        filterChain.doFilter(request, response);
+    }
+
+    
+}
